b790bf66ab699e33b7e30187fd18cbb3209ce57c
Trevor Kimenye <kimenye@gmail.com>
2015-03-03 09:39:39 +0300

    Created registration api

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 Usage
 =====
 
+Install composer
+curl -sS https://getcomposer.org/installer | php
+
 php ongair.php 254733171036 +123456789012345= Sprout
---
+++ index.php
@@ -0,0 +1,88 @@
+<?php
+
+require 'vendor/autoload.php';
+require_once('lib/phpdotenv/Dotenv.php');
+require_once('lib/whatsapp/whatsprot.class.php');
+
+Dotenv::load(__DIR__);
+
+$app = new \Slim\Slim();
+$app->view(new \JsonApiView());
+$app->add(new \JsonApiMiddleware());
+
+// Application status
+$app->get('/status', function () use ($app) {
+  $app->render(200, array(
+    'running' => true));
+});
+
+// Request an SMS code
+$app->post('/request', function() use ($app) {
+
+  $username = $app->request->params('phone_number');
+  $nickname = $app->request->params('nickname');
+  $carrier = $app->request->params('carrier');
+  $debug = $app->request->params('debug') == 'true';
+  $identity = $username;
+  $message = null;
+  $error = false;
+
+  if ($debug) {        
+    $message = 'Code requested';
+  }
+  else {
+    try {
+      $w = new WhatsProt($username, $identity, $nickname, false);
+      $w->codeRequest('sms', $carrier);
+    } 
+    catch(Exception $ex) {
+      $message = $ex->getMessage();
+      $error = true;
+    }
+  }
+
+  $app->render(200, array(
+    'error' => $error,
+    'message' => $message
+  ));
+
+});
+
+$app->post('/register', function() use ($app) {
+
+  $username = $app->request->params('phone_number');
+  $nickname = $app->request->params('nickname');
+  $code = $app->request->params('code');
+  $debug = $app->request->params('debug') == 'true';
+  $identity = $username;
+  $message = null;
+  $password = null;
+  $error = false;
+
+  if ($debug) {
+    $password = "1234567890wa+";
+  }
+  else {
+
+    $w = new WhatsProt($username, $identity, $nickname, false);
+    try {
+      $result = $w->codeRegister($code);
+      $password = $result->pw;
+    } 
+    catch(Exception $e) {
+      $message = $e->getMessage();
+    }
+  }
+
+  $app->render(200, array(
+    'error' => $error,
+    'message' => $message,
+    'password' => $password
+  ));
+
+});
+
+
+
+
+$app->run();
