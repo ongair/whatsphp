d67c6862bf736d570ade54c428ceff6ee2f49278
Trevor Kimenye <kimenye@gmail.com>
2015-03-11 11:58:54 +0300

    using composer autoload

--- ongair.php
+++ ongair.php
@@ -2,18 +2,19 @@
 
   require_once('lib/whatsapp/whatsprot.class.php');
   require_once('lib/activerecord/ActiveRecord.php');
-  require_once('lib/requests/Requests.php');
-  require_once('lib/phpdotenv/Dotenv.php');  
+  require_once('lib/requests/Requests.php');  
   require_once('client.php');
   require_once('util.php');  
+  require 'vendor/autoload.php';
   
   Requests::register_autoloader();
   Dotenv::load(__DIR__);
 
-  $debug = false;
   $username = $argv[1];
   $password = $argv[2];
   $nickname = $argv[3];
 
+
   $client = new Client($username, $password, $nickname);
   $client->loop();
+
