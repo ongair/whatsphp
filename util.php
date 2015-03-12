3420f9d03806fc0edf4147405b31519defadb4b9
Trevor Kimenye <kimenye@gmail.com>
2015-03-12 17:52:58 +0300

    removed unecessary comment'

--- util.php
+++ util.php
@@ -20,8 +20,6 @@
 
   function post_data($url, $data)
   {
-    // $url = 'http://0.0.0.0:3000/messages';
-    // $data = array('account' => $me, 'message' => array( 'text' => $body, 'phone_number' => get_phone_number($from), 'message_type' => 'Text', 'whatsapp_message_id' => $id, 'name' => $name) );
     $headers = array('Content-Type' => 'application/json', 'Accept' => 'application/json');
     Requests::post($url, $headers, json_encode($data));
   }
