fe1fab61708d78e7398ccf536b85ea99fab07abd
Trevor Kimenye <kimenye@gmail.com>
2015-07-20 11:06:54 +0300

    Made find service via phone_number faster

--- util.php
+++ util.php
@@ -99,8 +99,8 @@
 
   function service_from_phone_number($phone_number, $grep='') {
     if ($grep == '')
-      $grep = shell_exec('grep '.$phone_number.' /etc/init/whatsapp*');
-    preg_match("/\/etc\/init\/whatsapp\S*.conf/", $grep, $matches);
+      $grep = shell_exec('grep '.$phone_number.' /etc/init/whatsapp-*');
+    preg_match("/\/etc\/init\/whatsapp-\S*.conf/", $grep, $matches);
     return $matches[0];
   }
 
