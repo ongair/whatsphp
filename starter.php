198c6b4f2b6b726080c70d364b8c0e38a35e55dc
Trevor Kimenye <kimenye@gmail.com>
2015-07-20 11:55:52 +0300

    Fixed starter

--- starter.php
+++ starter.php
@@ -7,14 +7,18 @@
   Dotenv::load(__DIR__);
 
   _init_db();
-
+  $count = 0;
   $accounts = Account::all(array('setup' => true));
   foreach ($accounts as $account) {
     $number = $account->phone_number;
-    if (!service_running($number)) {
+    if (!service_running($number) && $count < 5) {
      	$service_name = service_from_phone_number($number);
 	    $service = service_name($service_name);
 
       echo "Account: ".$account->name." - ".$account->phone_number."  needs to start : ".$service.PHP_EOL;      
+      $running = shell_exec('service '.$service.' start');
+      echo "Account status: ".$running;
+      sleep(5);
+      $count++;
     }
   }
